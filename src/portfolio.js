1894a0e3679ac7b529c5f5ed70ed5b07ac44f2ad
Atharv Atre <91801614+atharvatre@users.noreply.github.com>
2024-03-10 20:19:38 +0530

    Update resume link in portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs / Nextjs and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1CYutqElbN5QDZ_kJbhIKp5IN-v1hQqoK/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/15la4lKpGjcnrMp6ebnu5VYFD-7FoEiiq/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
